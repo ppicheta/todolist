1291b87a1e83f2813187a2f2b54f95838c81bb16
ppicheta <77509567+ppicheta@users.noreply.github.com>
2023-09-16 20:59:52 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-file_path = r'C:\Projects\TodoList_app\todos.txt'
+file_path = 'todos.txt'
 
 
 def get_todos(file_path=file_path):
@@ -7,16 +7,8 @@ def get_todos(file_path=file_path):
     return todos
 
 
-def write_todos(arg, file_path):
+def write_todos(arg, file_path=file_path):
     with open(file_path, 'w') as file:
         file.writelines(arg)
     return file
-
-
-def greetings():
-    print('Hello')
-
-
-if __name__ == "__main__":
-    greetings()
 
